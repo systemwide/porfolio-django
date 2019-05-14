6a0e29c22c80a5b82a4ead5684200252d3e183e1
systemwide <benrotolo@gmail.com>
2019-05-14 18:19:55 -0400

    fleshed out jobs model, added blog model

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
 
-# Register your models here.
+# Register your models here.B
+from .models import Blog
+
+admin.site.register(Blog)
--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+
+class Blog(models.Model):
+    image = models.ImageField(upload_to='images/')
+    title = models.CharField(max_length=200)
+    pub_date = models.DateTimeField(auto_now=False)
+    content = models.TextField()
